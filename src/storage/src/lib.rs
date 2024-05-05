2ee658fb0c2d6be144b3e7ea4b2d3a9161c6986e
Suhail Rehman <suhailrehman@gmail.com>
2024-05-05 15:54:46 -0500

    enable all crates

--- src/storage/src/lib.rs
+++ src/storage/src/lib.rs
@@ -1,5 +1,5 @@
 mod storage_common;
 
-// pub use memstore::storage_manager::{StorageManager, STORAGE_DIR};
+//pub use memstore::storage_manager::{StorageManager, STORAGE_DIR};
 // pub use sledstore::storage_manager::{StorageManager, STORAGE_DIR};
 pub use heapstore::storage_manager::{StorageManager, STORAGE_DIR};
